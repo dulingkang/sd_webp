8e519558ddfc66655ecc9436fae145f22feb99d6
dulingkang@163.com <dulingkang@163.com>
2016-12-05 10:29:58 +0800

    add missing category, 3.8.3

--- sd_webpDemo/sd_webpDemo/ViewController.m
+++ sd_webpDemo/sd_webpDemo/ViewController.m
@@ -24,16 +24,18 @@
     [[SDImageCache sharedImageCache] cleanDisk];
     UIImageView *imageview = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
     
-    [imageview sd_setImageWithURL:[NSURL URLWithString:@"https://devthinking.com/images/animated.webp"]];
+    NSString *str1 = @"https://devthinking.com/images/animated.webp";
+    str1 = @"https://img.dmall.com/mIndex/201612/a3f819c6-f055-46f0-bd56-4586830bdc45_240x240H.webp";
+    [imageview sd_setImageWithURL:[NSURL URLWithString:str1]];
     imageview.contentMode = UIViewContentModeScaleAspectFit;
     [self.view addSubview:imageview];
     
-    UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(200, 0, 200, 200)];
-    [imageView1 sd_setImageWithURL:[NSURL URLWithString:@"https://devthinking.com/images/test1.webp"]];
-    imageView1.contentMode = UIViewContentModeScaleAspectFit;
-    [self.view addSubview:imageView1];
+//    UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(200, 0, 200, 200)];
+//    [imageView1 sd_setImageWithURL:[NSURL URLWithString:@"https://devthinking.com/images/test1.webp"]];
+//    imageView1.contentMode = UIViewContentModeScaleAspectFit;
+//    [self.view addSubview:imageView1];
     
-    [self addWebView];
+//    [self addWebView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
